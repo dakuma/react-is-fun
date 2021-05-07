696f2a7f0f3ebd486e071067d30f3590fd980309
dakuma <alienabductionsfordummies@hotmail.com>
2021-05-07 10:13:42 -0400

    Breaking code since 1999

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      &copy; 2021 React Revolutionists!
+    </div>
+  )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.css'
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum neque minima repellat temporibus maxime itaque ipsum cupiditate provident dolore eveniet laborum voluptate iusto, possimus commodi adipisci delectus in libero soluta.</p>
+    </div>
+  )
+}
